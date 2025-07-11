f10a702d7c986162678362012edd49e701302c3c
Arbel Arad <arbel@spacetime.technology>
2025-07-11 09:29:50 +0300

    add substitutes config

---
+++ flake/config.nix
@@ -0,0 +1,10 @@
+_: {
+  extra-substituters = [
+    "http://buildnix.spacetime.technology:8000"
+    "https://attic.spacetime.technology/buildnix"
+  ];
+  extra-trusted-public-keys = [
+    "buildnix.spacetime.technology:cUI+2I7OJ/ufQg6Or2NP7mzPwdLQ7LUny80UxyFr25A="
+    "buildnix:Ns8cOyVRHbn/FIui31sgg7b0LG4wNl+GmcOdTw8B73o="
+  ];
+}
