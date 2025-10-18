604b0ba856070d716ac36f3337d2dd173a99a807
Arbel Arad <arbel@spacetime.technology>
2025-10-18 23:10:29 +0300

    add meta description

--- flake/apps.nix
+++ flake/apps.nix
@@ -3,6 +3,16 @@
   "nvim.nix" = {
     type = "app";
     program = self'.packages.default;
+    meta = {
+      description = "Neovim";
+    };
   };
-  gui = import (self + /packages/nvf-wrapped.nix) { inherit pkgs; nvf = self'.packages.default; };
+  gui = (import (self + /packages/nvf-wrapped.nix) {
+    inherit pkgs;
+    nvf = self'.packages.default;
+  }) // {
+    meta = {
+      description = "Neovide GUI";
+    };
+  };
 }
