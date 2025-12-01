d6083ff295aa4d815f55e8d346afb017b3461a40
Arbel Arad <arbel@spacetime.technology>
2025-12-01 18:22:12 +0200

    test build & cache

---
+++ tools/build-cache.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+CACHE="buildnix"
+
+DEFAULT_P=$(nix build .#packages.x86_64-linux.default --no-link --json --quiet 2>/dev/null | jq '.[].outputs.out' -r)
+MINIMAL_P=$(nix build .#packages.x86_64-linux.nvim-minimal --no-link --json --quiet 2>/dev/null | jq '.[].outputs.out' -r)
+GUI_P=$(nix build .#packages.x86_64-linux.nvim-gui --no-link --json --quiet 2>/dev/null | jq '.[].outputs.out' -r)
+
+attic push "$CACHE" "$DEFAULT_P" || true
+attic push "$CACHE" "$MINIMAL_P" || true
+attic push "$CACHE" "$GUI_P" || true
+
+printf "full size:%s\n" "$(nix path-info -h -S -s "$GUI_P" | cut -f 2,3)"
+printf "minimal size:%s\n" "$(nix path-info -h -S -s "$MINIMAL_P" | cut -f 2,3)"
