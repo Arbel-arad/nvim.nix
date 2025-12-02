f63e1a5bfb68ef0f0e7d19ee036801fb66a33398
Arbel Arad <arbel@spacetime.technology>
2025-12-02 11:14:37 +0200

    print first, ask questions later

--- tools/build-cache.sh
+++ tools/build-cache.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-set -x
 set -euo pipefail
 
 CACHE="buildnix"
@@ -9,9 +8,10 @@ DEFAULT_P=$(nix build .#packages.x86_64-linux.default --no-link --json --quiet 2
 MINIMAL_P=$(nix build .#packages.x86_64-linux.nvim-minimal --no-link --json --quiet 2>/dev/null | jq '.[].outputs.out' -r)
 GUI_P=$(nix build .#packages.x86_64-linux.nvim-gui --no-link --json --quiet 2>/dev/null | jq '.[].outputs.out' -r)
 
+printf "full size:%s\n" "$(nix path-info -h -S -s "$GUI_P" | cut -f 2,3)"
+printf "minimal size:%s\n" "$(nix path-info -h -S -s "$MINIMAL_P" | cut -f 2,3)"
+
 attic push "$CACHE" "$DEFAULT_P" || true
 attic push "$CACHE" "$MINIMAL_P" || true
 attic push "$CACHE" "$GUI_P" || true
 
-printf "full size:%s\n" "$(nix path-info -h -S -s "$GUI_P" | cut -f 2,3)"
-printf "minimal size:%s\n" "$(nix path-info -h -S -s "$MINIMAL_P" | cut -f 2,3)"
