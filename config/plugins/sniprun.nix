3dff3d8b0d9e1d5db6a82830a494b84088583ed5
Arbel Arad <arbel@spacetime.technology>
2025-12-08 00:40:05 +0200

    sniprun works for bash but not lua (REPL mode?)

--- config/plugins/sniprun.nix
+++ config/plugins/sniprun.nix
@@ -14,22 +14,43 @@ in {
 
         package = pkgs.vimPlugins.sniprun;
 
-        setupOpts = if !enableExtra then {} else {
+        setupModule = "sniprun";
+
+        setupOpts = {
           repl_enable = [
             "Rust_original"
           ];
 
+          display = [
+            "VirtualLineOk"
+            "TempFloatingWindowErr"
+            "NvimNotify"
+          ];
+
+          display_options = {
+            terminal_position = "horizontal";
+            notification_timeout = 10;
+          };
+
           interpreter_options = {
             Rust_original = {
-              compiler = "${lib.getExe pkgs.rustc}";
+              compiler = "${pkgs.rustc}/bin/rustc";
             };
 
             TypeScript_original = {
-              interpreter = "${lib.getExe pkgs.node}";
+              interpreter = "${lib.getExe pkgs.nodejs}";
             };
 
             SQL_original = {
               interpreter = "${lib.getExe pkgs.usql}";
+            };
+
+            OrgMode_original = {
+              default_filetype = "bash";
+            };
+
+            Neorg_original = {
+              default_filetype = "bash";
             };
           };
         };
