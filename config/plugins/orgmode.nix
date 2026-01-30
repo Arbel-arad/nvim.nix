1982b4bc4504f94bd9ccf1ba11a540b50ae2a74a
Arbel Arad <arbel@spacetime.technology>
2026-01-31 01:00:43 +0200

    more weird issues with orgmode

--- config/plugins/orgmode.nix
+++ config/plugins/orgmode.nix
@@ -2,15 +2,40 @@
 
   enabled = nvimSize <= 400;
 
-  inherit (nvf.lib.nvim) dag;
+  inherit (nvf.lib.nvim) lua dag;
 
   org_priority = 100;
 
+  config = {
+    orgmode = {
+
+    };
+
+    org-roam = {
+      directory = "~/.org-roam";
+    };
+  };
+
 in {
+  startPlugins = with pkgs.vimPlugins; [
+    orgmode
+    #org-roam-nvim
+  ];
+
+  pluginRC = {
+    orgmode = dag.entryAnywhere /* lua */ ''
+        require("orgmode").setup(${lua.toLuaObject config.orgmode})
+        --[[
+          require("org-roam").setup(${lua.toLuaObject config.org-roam})
+        ]]
+    '';
+  };
+
   lazy = {
     plugins = {
-      orgmode = {
-        inherit enabled;
+      orgmode = lib.mkIf false {
+        #inherit enabled;
+        enabled = false;
 
         priority = org_priority + 10;
 
@@ -45,8 +70,8 @@ in {
         ];
       };
 
-      "org-roam.nvim" = {
-        inherit enabled;
+      "org-roam.nvim" = lib.mkIf true {
+        #inherit enabled;
         #enabled = false;
 
         priority = org_priority + 5;
@@ -57,12 +82,12 @@ in {
 
         setupOpts = {
           directory = "~/.org-roam";
-          org_files = [
+          #org_files = [
 
-          ];
+          #];
         };
 
-        lazy = false;
+        lazy = true;
 
         ft = [
           "org"
@@ -70,10 +95,11 @@ in {
 
         cmd = [
           "Org"
+          "RoamReset"
         ];
 
         event = [
-          "BufEnter"
+          #"BufEnter"
         ];
 
 
