84cecf33c6bad2072a26eca05a8f89f095dce1b2
Arbel Arad <arbel@spacetime.technology>
2026-02-16 16:03:09 +0200

    include spell lists in repo

--- config/spellcheck.nix
+++ config/spellcheck.nix
@@ -17,13 +17,13 @@ in {
   additionalRuntimePaths = [
     ./plugins/spell-lists
 
-    (builtins.path {
-      name = "spell-files";
-      path = pkgs.linkFarm "spell" [
-        (mkSpell "en.utf-8.spl" "0w1h9lw2c52is553r8yh5qzyc9dbbraa57w9q0r9v8xn974vvjpy")
-        (mkSpell "he.utf-8.spl" "12q7vw7fiwp7fw708df6bad0dajcfasq5vg0kp2y4vmaiibzv48v")
-      ];
-    })
+      #(builtins.path {
+      #  name = "spell-files";
+      #  path = pkgs.linkFarm "spell" [
+      #    (mkSpell "en.utf-8.spl" "0w1h9lw2c52is553r8yh5qzyc9dbbraa57w9q0r9v8xn974vvjpy")
+      #    (mkSpell "he.utf-8.spl" "12q7vw7fiwp7fw708df6bad0dajcfasq5vg0kp2y4vmaiibzv48v")
+      #  ];
+      #})
   ];
 
   spellcheck = {
