2812d6c3f0d8054551bab0f8d6932730294ca097
Arbel Arad <arbel@spacetime.technology>
2026-06-11 17:35:16 +0300

    fpga tool cleanup

--- config/embedded/fpga.nix
+++ config/embedded/fpga.nix
@@ -2,22 +2,16 @@
 
   enabled = nvimSize < 500;
 
-  toolchains = import npins.fpga-toolchains { inherit pkgs lib; };
+  toolchains = import npins.fpga-toolchains {
+    inherit pkgs lib;
+  };
 
 in {
-  extraPackages = lib.optionals enabled ([
-    pkgs.openfpgaloader
-
-    pkgs.nextpnrWithGui
-    pkgs.nextpnr-xilinx
-
-    pkgs.yosys
-    pkgs.sby
-    pkgs.mcy
-
-  ] ++ toolchains.packagesFor [
+  extraPackages = lib.optionals enabled (
+    toolchains.packagesFor [
       "xilinx-XC7"
       "lattice-iCE40"
       "lattice-ECP5"
-  ]);
+    ]
+  );
 }
