0a2309896047d3d97f39ed4686579ba4e9c3f899
Arbel Arad <arbel@spacetime.technology>
2026-09-12 15:11:02 +0300

    use new attic instance

--- flake.nix
+++ flake.nix
@@ -229,7 +229,7 @@
     ];
     extra-trusted-public-keys = [
       "buildnix.spacetime.technology:cUI+2I7OJ/ufQg6Or2NP7mzPwdLQ7LUny80UxyFr25A="
-      "buildnix:Ns8cOyVRHbn/FIui31sgg7b0LG4wNl+GmcOdTw8B73o="
+      "buildnix:qoIQsNnowGUD+xY+ULAtgp0tRXpEi4JO0191uHu869E="
     ];
 
   };
